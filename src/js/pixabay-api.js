830e49a2f3900f7f1819fc13fbf3323c0992a8fb
julia <julia.zhyvylo@asila.mk>
2024-12-10 20:52:29 +0100

    add api

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+const params = new URLSearchParams({
+  key: '47568770-df67670bdc8e06478abf0c27f',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+});
+
+const url = `https://pixabay.com/api/?${params}`;
+
+export default function getImages(query) {
+  return fetch(`${url}&q=${query}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
