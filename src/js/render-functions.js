b3ee2bd30545e817467ad9f7c56decd094be0258
julia <julia.zhyvylo@asila.mk>
2024-12-13 19:34:28 +0100

    add styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,27 +7,25 @@ export const createCard = ({
   comments,
   downloads,
 }) => {
-  return `<li class='images-item'>
-  <div class='card-wrapper'>
-  
+  return `<li class='image-item'>
+    <div class='card-wrapper'>
+      <div class='image-wrapper'>
         <a class='card-link' href=${largeImageURL}>
-          <img src=${webformatURL}>
-          </a>
-        </div>
-        <div class='card-content'>
-          <ul class='stats-list'>
-            <li class='stats-item'><div><span>Likes</span><span>${likes}</span></div></li>
-            <li class='stats-item'><div><span>Views</span><span>${views}</span></div></li>
-            <li class='stats-item'><div><span>Comments</span><span>${comments}</span></div></li>
-            <li class='stats-item'><div><span>Downloads</span><span>${downloads}</span></div></li>
-          </ul>
-        </div>
-      
+          <img src=${webformatURL} alt='${tags}'>
+        </a>
+      </div>
+      <div class='card-content'>
+        <ul class='stats-list'>
+          <li class='stats-item'><div><span>Likes</span><span>${likes}</span></div></li>
+          <li class='stats-item'><div><span>Views</span><span>${views}</span></div></li>
+          <li class='stats-item'><div><span>Comments</span><span>${comments}</span></div></li>
+          <li class='stats-item'><div><span>Downloads</span><span>${downloads}</span></div></li>
+        </ul>
+      </div>
+    </div>
   </li>`
 };
 
 export const appendCards = (ulElem, cards) => {
   ulElem.insertAdjacentHTML('afterbegin', cards);
 }
-
-// export default {createCard, appendCards};
--- src/main.js
+++ src/main.js
@@ -7,38 +7,54 @@ import "izitoast/dist/css/iziToast.min.css";
 import getImages from './js/pixabay-api';
 import {createCard, appendCards}  from './js/render-functions';
 
-const formRef = document.querySelector('.search-form')
+const formRef = document.querySelector('.search-form');
+const inputRef = document.querySelector('.search-input')
 const ulElemRef = document.querySelector('.images-list');
+const loaderRef = document.querySelector('.loader');
+
 formRef.addEventListener('submit', (e) => handleSubmit(e))
 
 function handleSubmit(e) {
   e.preventDefault();
 
-  const query = e.target.search.value.split(' ').join('+');
+  if(e.target.search.value.trim()) {
+    inputRef.style.borderColor = '#808080';
+    ulElemRef.innerHTML = '';
 
-  getImages(query)
-    .then(result => {
-      if(result.hits.length) {
-        const cards = result.hits.map(hit => createCard(hit)).join("");
-        appendCards(ulElemRef, cards);
-        gallery.refresh();
-      } else {
-        iziToast.show({
-          title: 'Warning',
-          message: "Sorry, there are no images matching your search query. Please try again!",
-          position: 'topRight',
-          color: 'yellow',
-        })
-      }
-    })
-    .catch(error => {
-      iziToast.show({
-        title: 'Error',
-        message: "Sorry, trouble happend. Please try again later!",
-        position: 'topRight',
-        color: 'red',
+    const query = e.target.search.value.trim().split(' ').join('+');
+
+    getImages(query)
+      .then(result => {
+        if(result.hits.length) {
+          const cards = result.hits.map(hit => createCard(hit)).join("");
+          appendCards(ulElemRef, cards);
+          gallery.refresh();
+        } else {
+          iziToast.show({
+            title: 'Warning',
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            position: 'topRight',
+            color: 'yellow',
+          })
+        }
       })
-    });
+      .catch(error => {
+        iziToast.show({
+          title: 'Error',
+          message: "Sorry, trouble happend. Please try again later!",
+          position: 'topRight',
+          color: 'red',
+        })
+      });
+  } else {
+    inputRef.style.borderColor = 'red';
+    iziToast.show({
+      title: 'Warning',
+      message: "Please fill the field!",
+      position: 'topRight',
+      color: 'red',
+    })
+  }
 }
 
 let gallery = new SimpleLightbox('.card-link', {
